6435e8d63b92bd8c4ed5303f0889bc360b99eddd
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-03-17 13:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,14 @@
 let name = document.getElementById('name');
 let age = document.getElementById('age');
 
-function voter(){
-    
+function success(){
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
         if(age.value > 18){
-            resolve(`${alert("Welcome ,"+" "+ name.value+".You can vote.")}`)
+            resolve(`${alert("Welcome,"+" "+ name.value+"."+" "+"You can vote.")}`)
         }else{
-            reject(`${alert("Oh sorry ,"+" "+ name.value+".You aren't old enough.")}`)
+            reject(`${alert("Oh sorry"+ name.value+". You aren't old enough.")}`)
         }
     },4000)
     })
-   
 }
